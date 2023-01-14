6e8788844ff7743ac61a46f939d390089e276f79
Matheus1221 <58350861+Matheus1221@users.noreply.github.com>
2023-01-14 15:40:59 -0300

    Mudança na api de imagem dos paises/ estilização finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-//dc78d17ef8d7788bc6cb14d5a59c28ab
+// Var and elements
+const apikey = "dc78d17ef8d7788bc6cb14d5a59c28ab";
+const apiCountryURL = "https://flagsapi.com//flat/64.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+//functions
+
+const getWeatherData = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&unitis=metric&appid=${apikey}&lang=pt_br`;
+
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+  console.log(data);
+};
+
+const showWeatherData = (city) => {
+  getWeatherData(city);
+};
+
+//events
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
